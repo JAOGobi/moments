202fdd4a9eb78cb1dc11d6cd2ec00e2081583415
João Gobi <joao_gobi2012@hotmail.com>
2022-10-27 16:00:50 -0300

    Resgatando dados do banco

--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -3,8 +3,10 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Moment } from '../Moment';
+import { Response } from '../Response';
 
 import { environment } from 'src/environments/environment';
+import { Form } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +16,10 @@ export class MomentService {
   private apiUrl =`${this.baseApiUrl}api/moments`;
   
   constructor(private http: HttpClient) { }
+
+    getMoments(): Observable<Response<Moment[]>> {
+      return this.http.get<Response<Moment[]>>(this.apiUrl);
+    }
 
   createMoment(formData: FormData): Observable<FormData>{
     return this.http.post<FormData>(this.apiUrl, formData);
