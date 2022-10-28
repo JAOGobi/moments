37b8f52a30adc2a1df0c86a50e946ad6f22ea5e5
João Gobi <joao_gobi2012@hotmail.com>
2022-10-28 09:11:10 -0300

    Busca de dados no Angular

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -15,10 +15,12 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
   allMoments: Moment[] =[]
   moments: Moment[] = []
   baseApiUrl = environment.baseApiUrl
+
+  faSearch = faSearch;
+  searchTerm: string ='';
 
   constructor(private momentService: MomentService) { }
 
@@ -36,4 +38,12 @@ export class HomeComponent implements OnInit {
     })
   }
 
+  search(e: Event): void{
+    const target = e.target as HTMLInputElement;
+    const value = target.value;
+
+    this.moments = this.allMoments.filter((moment) => {
+      return moment.title.toLowerCase().includes(value);
+    });
+  }
 }
